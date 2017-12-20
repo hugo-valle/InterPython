fbb045fc7afc31a576ac64d7934cd5949f67a1a6
Hugo Valle <hugovalle1@weber.edu>
2017-12-20 16:11:28 -0700

    First commit

--- challenge3.py
+++ challenge3.py
@@ -1,12 +1,18 @@
 import re
 
 def find_message():
-    pattern = re.compile(r"[A-Z]{3}")
+    pattern = re.compile(r"[^A-Z][A-Z]{3}[a-z][A-Z]{3}[^A-Z]")
     with open("challenge3_data.hmtl", mode='rt', encoding='utf-8') as data:
         # Loop over file
+        count = 1
         for rec in data:
-            hit = re.search(pattern, rec)
-            print(hit)
+            # See re.math(), re.search(), re.findall()
+            hit = re.findall(pattern, rec)
+            for h in hit:
+                print(h[4], end='')
+                count += 1
+        print()
+        print("Found {} hits".format(count))
 
 if __name__ == '__main__':
     find_message()
